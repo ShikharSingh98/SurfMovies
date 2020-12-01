e70b989e91728ccabbe6048e4b6af0b676fdd771
ShikharSingh98 <shikharsingh98@gmail.com>
2020-12-01 13:17:28 +0530

    Created Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import styles from './App.module.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './Navbar.module.css';
+import { ReactComponent as TMDBLogo } from '../../assets/TMDB Logo.svg';
+
+function Navbar() {
+  console.log(styles);
+  return (
+    <div className={styles.container}>
+      <div className={styles.brand}>
+        <img src="https://img.icons8.com/nolan/48/movie.png" alt="brand-logo" />
+        <span className={styles.brandName}>SurfMovies</span>
+      </div>
+      <TMDBLogo className={styles.logo} />
+    </div>
+  );
+}
+
+export default Navbar;
